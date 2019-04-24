e68106ada9ae706e77114617b75ee27cb9e94ef4
mohammed.shakeel <mohammed.shakeel@NB-SHAKEELA2082.sga.com>
2019-04-24 09:24:13 +0530

    Search Cancer

---
+++ Object Repository/Cancer Search/Page_H1 Institution and Expert Profiles Opinion Leader Identification and Analysis and Scholarly Analytics/input_Name_sc-kAzzGY fXHSOY sc-cSHVUG bVKDce.rs
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Name_sc-kAzzGY fXHSOY sc-cSHVUG bVKDce</name>
+   <tag></tag>
+   <elementGuidId>6925957e-440d-4de6-9b52-8558268ad421</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@value='']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sc-kAzzGY fXHSOY sc-cSHVUG bVKDce</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>placeholder</name>
+      <type>Main</type>
+      <value>Search for drugs, indications, targets, and other scientific keywords</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/span[1]/div[@class=&quot;sc-iqzUVk gBjIgX&quot;]/div[@class=&quot;sc-ipZHIp dlkTGJ&quot;]/div[@class=&quot;sc-iGrrsa jMJMrF&quot;]/div[1]/div[1]/form[1]/div[1]/div[@class=&quot;sc-fONwsr hzCAfP&quot;]/div[2]/div[1]/input[@class=&quot;sc-kAzzGY fXHSOY sc-cSHVUG bVKDce&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//input[@value='']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='app']/span/div[2]/div/div/div/div/form/div/div/div[2]/div/input</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Name'])[1]/following::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Keyword'])[1]/following::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/preceding::input[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sort by:'])[1]/preceding::input[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//input</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/Cancer_Search/Script1556077054968.groovy
@@ -0,0 +1,35 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://develop-curie.h1insights.com/welcome')
+
+WebUI.click(findTestObject('Object Repository/Cancer Search/Page_H1 Institution and Expert Profiles Opinion Leader Identification and Analysis and Scholarly Analytics/button_Log in with Okta'))
+
+WebUI.setText(findTestObject('Object Repository/Cancer Search/Page_H1-dev-859455 - Sign In/input_Sign In_username'), 'admin2@h1insights.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Cancer Search/Page_H1-dev-859455 - Sign In/input_Sign In_password'), 
+    'CeXXbuXHh3rd9oWlMamFYA==')
+
+WebUI.click(findTestObject('Object Repository/Cancer Search/Page_H1-dev-859455 - Sign In/input_Remember me_okta-signin-submit'))
+
+WebUI.click(findTestObject('Object Repository/Cancer Search/Page_H1 Institution and Expert Profiles Opinion Leader Identification and Analysis and Scholarly Analytics/div_Search'))
+
+WebUI.setText(findTestObject('Object Repository/Cancer Search/Page_H1 Institution and Expert Profiles Opinion Leader Identification and Analysis and Scholarly Analytics/input_Name_sc-kAzzGY fXHSOY sc-cSHVUG bVKDce'), 
+    'cancer')
+
+WebUI.click(findTestObject('Object Repository/Cancer Search/Page_H1 Institution and Expert Profiles Opinion Leader Identification and Analysis and Scholarly Analytics/div_Search'))
+
