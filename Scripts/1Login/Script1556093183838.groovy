05955e2d7bc4546a6d21e2ef0f862a37365fa790
mohammed.shakeel <mohammed.shakeel@NB-SHAKEELA2082.sga.com>
2019-04-24 13:38:24 +0530

    First Login

---
+++ Scripts/1Login/Script1556093183838.groovy
@@ -0,0 +1,27 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://develop-curie.h1insights.com/')
+
+WebUI.click(findTestObject('Cancer Search/Page_H1 Institution and Expert Profiles Opinion Leader Identification and Analysis and Scholarly Analytics/button_Log in with Okta'))
+
+WebUI.setText(findTestObject('Cancer Search/Page_H1-dev-859455 - Sign In/input_Sign In_username'), 'admin2@h1insights.com')
+
+WebUI.setEncryptedText(findTestObject('Cancer Search/Page_H1-dev-859455 - Sign In/input_Sign In_password'), 'CeXXbuXHh3rd9oWlMamFYA==')
+
+WebUI.click(findTestObject('Cancer Search/Page_H1-dev-859455 - Sign In/input_Remember me_okta-signin-submit'))
+
